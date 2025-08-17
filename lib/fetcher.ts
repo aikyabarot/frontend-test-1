e70f071e9bf388d26d37ee46379fa2bf8feb7676
aikyabarot <71308687+aikyabarot@users.noreply.github.com>
2025-08-17 11:29:55 +0530

    fix: correct leading slash removal in fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -3,7 +3,9 @@ import { getEnv } from "./env";
 
 export async function fetchJSON<T>(path: string): Promise<T> {
   const { NEXT_PUBLIC_API_BASE } = getEnv();
-  const url = `${NEXT_PUBLIC_API_BASE.replace(/\/$/, "")}/${path.replace(/\^\//, "")}`;
+  const base = NEXT_PUBLIC_API_BASE.replace(/\/$/, "");
+  const cleaned = path.replace(/^\/+ , "");
+  const url = `${base}/${cleaned}`;
   const res = await axios.get<T>(url, { headers: { Accept: "application/json" } });
   return res.data;
 }
